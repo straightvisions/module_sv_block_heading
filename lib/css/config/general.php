5000cc371ac2c1021e5943ee96715b7ff930f6f6
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-09-21 19:35:37 +0200

    MIGRATE TO V5.100

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -4,13 +4,13 @@
 		echo $_s->build_css(
 			is_admin() ? '.edit-post-visual-editor.editor-styles-wrapper h'.$i : '.sv100_sv_content_wrapper article h'.$i,
 			array_merge(
-				$script->get_parent()->get_setting('h'.$i.'_font_family')->get_css_data('font-family'),
-				$script->get_parent()->get_setting('h'.$i.'_font_size')->get_css_data('font-size','','px'),
-				$script->get_parent()->get_setting('h'.$i.'_line_height')->get_css_data('line-height'),
-				$script->get_parent()->get_setting('h'.$i.'_text_color')->get_css_data(),
-				$script->get_parent()->get_setting('h'.$i.'_padding')->get_css_data('padding'),
-				$script->get_parent()->get_setting('h'.$i.'_margin')->get_css_data(),
-				$script->get_parent()->get_setting('h'.$i.'_border')->get_css_data()
+				$module->get_setting('h'.$i.'_font_family')->get_css_data('font-family'),
+				$module->get_setting('h'.$i.'_font_size')->get_css_data('font-size','','px'),
+				$module->get_setting('h'.$i.'_line_height')->get_css_data('line-height'),
+				$module->get_setting('h'.$i.'_text_color')->get_css_data(),
+				$module->get_setting('h'.$i.'_padding')->get_css_data('padding'),
+				$module->get_setting('h'.$i.'_margin')->get_css_data(),
+				$module->get_setting('h'.$i.'_border')->get_css_data()
 			)
 		);
 
