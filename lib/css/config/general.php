8120a6de1189b0ffff834b66e6c4343c6bec516c
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-01-19 17:25:06 +0100

    UPDATE POST EDITOR CSS SELECTOR

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -2,7 +2,7 @@
 	$i = 1;
 	while ($i <= 6) {
 		echo $_s->build_css(
-			is_admin() ? '.edit-post-visual-editor.editor-styles-wrapper h'.$i : '.sv100_sv_content_wrapper article h'.$i,
+			is_admin() ? '.editor-styles-wrapper h'.$i : '.sv100_sv_content_wrapper article h'.$i,
 			array_merge(
 				$module->get_setting('h'.$i.'_font_family')->get_css_data('font-family'),
 				$module->get_setting('h'.$i.'_font_size')->get_css_data('font-size','','px'),
