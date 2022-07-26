36ab8afde6c4fd1f1ed92b63f682d6934114091b
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-07-26 16:25:30 +0200

    fix selector

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -4,18 +4,7 @@
 	
 	$i = 1;
 	while ($i <= 6) {
-		
-		$selector_editor =
-			'.editor-styles-wrapper h'.$i.'.wp-block-heading'
-			.', .editor-styles-wrapper h1.wp-block-heading.is-style-h'.$i
-			.', .editor-styles-wrapper h2.wp-block-heading.is-style-h'.$i
-			.', .editor-styles-wrapper h3.wp-block-heading.is-style-h'.$i
-			.', .editor-styles-wrapper h4.wp-block-heading.is-style-h'.$i
-			.', .editor-styles-wrapper h5.wp-block-heading.is-style-h'.$i
-			.', .editor-styles-wrapper h6.wp-block-heading.is-style-h'.$i
-			.', .editor-styles-wrapper p.is-style-h'.$i;
-		
-		$selector_frontend =
+		$selector =
 			'h'.$i
 			.', h1.is-style-h'.$i
 			.', h2.is-style-h'.$i
@@ -24,9 +13,19 @@
 			.', h5.is-style-h'.$i
 			.', h6.is-style-h'.$i
 			.', p.is-style-h'.$i;
+
+		$selector .=
+			', .editor-styles-wrapper h'.$i
+			.', .editor-styles-wrapper h1.is-style-h'.$i
+			.', .editor-styles-wrapper h2.is-style-h'.$i
+			.', .editor-styles-wrapper h3.is-style-h'.$i
+			.', .editor-styles-wrapper h4.is-style-h'.$i
+			.', .editor-styles-wrapper h5.is-style-h'.$i
+			.', .editor-styles-wrapper h6.is-style-h'.$i
+			.', .editor-styles-wrapper p.is-style-h'.$i;
 			
 		echo $_s->build_css(
-			is_admin() ? $selector_editor : $selector_frontend,
+			$selector,
 			array_merge(
 				$module->get_setting('h'.$i.'_hyphens')->get_css_data('-webkit-hyphens'),
 				$module->get_setting('h'.$i.'_hyphens')->get_css_data('-moz-hyphens'),
