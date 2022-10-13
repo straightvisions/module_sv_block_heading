4c4b8017b2bf2e7a3c81cbcf6abea87f3c864575
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-10-13 10:58:43 +0200

    fix styles & style registration

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,5 +1,4 @@
 <?php
-	
 	$i = 1;
 	while ($i <= 6) {
 		$selector =
@@ -10,8 +9,16 @@
 			.', .wp-site-blocks h4.is-style-h'.$i
 			.', .wp-site-blocks h5.is-style-h'.$i
 			.', .wp-site-blocks h6.is-style-h'.$i
-			.', .wp-site-blocks p.is-style-h'.$i;
-			
+			.', .wp-site-blocks p.is-style-h'.$i
+			.', .editor-styles-wrapper h'.$i
+			.', .editor-styles-wrapper h1.is-style-h'.$i
+			.', .editor-styles-wrapper h2.is-style-h'.$i
+			.', .editor-styles-wrapper h3.is-style-h'.$i
+			.', .editor-styles-wrapper h4.is-style-h'.$i
+			.', .editor-styles-wrapper h5.is-style-h'.$i
+			.', .editor-styles-wrapper h6.is-style-h'.$i
+			.', .editor-styles-wrapper p.is-style-h'.$i;
+
 		echo $_s->build_css(
 			$selector,
 			array_merge(
@@ -27,6 +34,6 @@
 				$module->get_setting('h'.$i.'_border')->get_css_data()
 			)
 		);
-	
+
 		$i++;
 	}
